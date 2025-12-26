89ae27e767186191a641b7c34a7ce43631eb11fb
Dhruv <dsirohi98@gmail.com>
2025-12-25 17:02:02 -0800

    add camera driver header

---
+++ cpp/camera_driver.hpp
@@ -0,0 +1,31 @@
+//
+// Created by Dhruv Sirohi on 12/25/25.
+//
+
+#ifndef CAMERA_DRIVER_HPP
+#define CAMERA_DRIVER_HPP
+
+#include <thread>
+
+#include "camera_frame.hpp"
+#include "ring_buffer.hpp"
+
+class CameraDriver {
+
+    CameraDriver(int camera_id, int height, int width);
+    ~CameraDriver();
+
+    void start();
+    void stop();
+
+private:
+    RingBuffer<CameraFrame> buffer_;
+    int camera_id_;
+    int height_;
+    int width_;
+    std::atomic<bool> running_;
+    std::thread worker_;
+    int fps_;
+
+};
+#endif //CAMERA_DRIVER_HPP
