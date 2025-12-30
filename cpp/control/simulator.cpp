9d0e4a3f5450f141137f2042e8d04ef47eb94bd0
Dhruv <dsirohi98@gmail.com>
2025-12-30 13:55:14 -0800

    add Simulator header and source

--- cpp/control/simulator.cpp
+++ cpp/control/simulator.cpp
@@ -1,3 +1,30 @@
 //
 // Created by Dhruv Sirohi on 12/30/25.
 //
+
+#include "simulator.hpp"
+
+Simulator::Simulator(double dt, VehicleModel model) :
+dt_(dt), model_(model) {}
+
+void Simulator::reset(const State &initial_state) {
+    state_ = initial_state;
+    trajectory_.clear();
+    trajectory_.push_back(state_);
+}
+
+void Simulator::step(const Control &u) {
+    state_ = model_.step(state_, u, dt_);
+    trajectory_.push_back(state_);
+}
+
+const State &Simulator::currentState() const {
+    return state_;
+}
+
+const std::vector<State> &Simulator::trajectory() const {
+    return trajectory_;
+}
+
+
+
--- cpp/control/simulator.hpp
+++ cpp/control/simulator.hpp
@@ -5,4 +5,26 @@
 #ifndef SIMULATOR_HPP
 #define SIMULATOR_HPP
 
+#include "vehicle_model.hpp"
+#include <vector>
+
+class Simulator {
+public:
+    Simulator(double dt, VehicleModel model);
+
+    void reset(const State& initial_state);
+
+    void step(const Control& u);
+
+    const State& currentState() const;
+    const std::vector<State>& trajectory() const;
+
+private:
+    double dt_;
+    VehicleModel model_;
+
+    State state_;
+    std::vector<State> trajectory_;
+};
+
 #endif //SIMULATOR_HPP
