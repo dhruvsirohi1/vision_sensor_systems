9288dbd2e23f757540b1a57b6270b9d8296b41f0
Dhruv <dsirohi98@gmail.com>
2025-12-30 01:21:24 -0800

    add virtual imu edits to main

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -5,10 +5,12 @@
 #include "camera_driver.hpp"
 #include "ring_buffer.hpp"
 #include<iostream>
+#include "virtual_imu.hpp"
 
 int main() {
     std::cout << "runnning..." << std::endl;
     RingBuffer<CameraFrame> camera_buffer(10);
+    RingBuffer<ImuMeasurement> imu_buffer(100);
 
     CameraDriver camera(
     /*camera_id=*/0,
@@ -18,11 +20,20 @@ int main() {
     camera_buffer
     );
 
+    VirtualImu virtual_imu(200.0, imu_buffer);
+
     camera.start();
+    virtual_imu.start();
 
     for (int i = 0; i < 100; i++) {
         CameraFrame frame = camera_buffer.pop();
+        ImuMeasurement imu_meas = imu_buffer.pop();
+
         std::cout << "Frame timestamp: " << frame.timestamp_ns << std::endl;
+        std::cout << " | IMU ts: " << imu_meas.timestamp_ns
+                  << " | gyro_z: " << imu_meas.gyro_z
+                  << std::endl;
+
         cv::imshow("Camera", frame.frame);
         if (cv::waitKey(30) == 27) {break;}
     }
